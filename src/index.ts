74a3a53ad99de9ab849e1d157f51d7d429fdec2f
Ismael Bernal <ismaelbernal83@gmail.com>
2021-08-29 18:00:08 +0200

    Project structure ready

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const port = 3000;
+const app = express();
+
+app.listen(3000, () => console.log(`Server listening at port http://localhost:${port}`));
+app.get('/', (req, res) => res.send('OK'));
